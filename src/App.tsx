05cc1a3488ab22060095de02b4af45b331bf07a2
jidogoon <jidohyun@gmail.com>
2021-04-13 02:00:57 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+import "@tensorflow/tfjs-backend-cpu";
+import "./App.css";
+import movie from "./SpolierFrontend.mp4";
+import { FaceDetector } from "./FaceDetector";
 
-function App() {
+const detector = new FaceDetector();
+
+const detectFaces = async (video: HTMLVideoElement) => {
+  const canvasElement = document.createElement("canvas") as HTMLCanvasElement;
+  canvasElement.width = video.videoWidth;
+  canvasElement.height = video.videoHeight;
+  const ctx = canvasElement.getContext("2d");
+  ctx?.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+  return await detector.getFaces(canvasElement);
+};
+
+const App = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [faceText, setFaceText] = useState("");
+
+  useEffect(() => {
+    setInterval(async () => {
+      if (!detector.isModelReady()) {
+        setFaceText("Model is not ready :(");
+        return;
+      }
+      const currentVideo = videoRef.current;
+      if (!currentVideo) return;
+      const before = Date.now();
+      const faces = await detectFaces(currentVideo);
+      const took = Date.now() - before;
+      const faceText = faces
+        ?.map((face) => {
+          return `[took ${took}ms] topLeft: ${face.topLeft} / bottomRight: ${
+            face.bottomRight
+          } / probability: ${face.probability ?? "0.0"}`;
+        })
+        ?.join();
+      setFaceText(faceText ?? "");
+    }, 500);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video ref={videoRef} controls autoPlay muted src={movie} />
+      <div>{faceText}</div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/FaceDetector.tsx
@@ -0,0 +1,19 @@
+import * as blazeface from "@tensorflow-models/blazeface";
+import * as tf from "@tensorflow/tfjs-core";
+
+export class FaceDetector {
+  _model: blazeface.BlazeFaceModel | undefined;
+  constructor() {
+    (async () => {
+      await tf.setBackend("cpu");
+      this._model = await blazeface.load();
+    })();
+  }
+  isModelReady = () => this._model != null;
+  getFaces = async (
+    source: HTMLVideoElement | HTMLCanvasElement | ImageData | undefined
+  ) => {
+    if (!source) return null;
+    return this._model?.estimateFaces(source);
+  };
+}
